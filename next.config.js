b4926c17376c752945a66305721b926219cc2b02
Mehmet Emre Doğan <med@mehmetemredogan.com.tr>
2021-08-21 19:40:47 +0300

    next.js upgrade

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const mode  = process.env.NODE_ENV === 'production'
 module.exports = {
     poweredByHeader: false,
     assetPrefix: mode ? 'http://localhost:3000' : '', // CDN URL
-    future: {
-        webpack5: true,
-    },
     compress: false, // Custom Server Mode Error Fix
     publicRuntimeConfig: {
         cookieDomain: process.env.COOKIE_DOMAIN,
